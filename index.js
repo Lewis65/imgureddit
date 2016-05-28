9709e66681b39fa3f13911565f207dbf628555bd
Lewis Horwood <horwood.lewis@gmail.com>
2016-05-27 19:49:15 -0600

    All done, I think

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ var mongo = require('mongodb').MongoClient,
 
 var bodyParser = require('body-parser');
 var request = require('request');
+var morgan = require('morgan');
 
+app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -45,18 +47,19 @@ function resultsHandler(req, res, args){
         } else {
             var results = JSON.parse(body);
             if(results.success){
+                //save the search to history
+                saveSearch(args);
                 resultsToPages(results.data, res, args);
             }
         }
     });
 }
 
-var pages;
-
+//move json results into pages for display
 function resultsToPages(data, res, args){
     var length = data.length;
     console.log(length);
-    pages = [[]];
+    var pages = [[]];
     for (var i = 0, k = 0; i < length; i++){
         if (i % 10 === 0 && pages[k]){
             k++;
@@ -74,12 +77,13 @@ function resultsToPages(data, res, args){
     showPage(pages, args, 0, res);
 }
 
+//display a page of results
 function showPage(pageArray, args, change, res){
     //add page change to offset
     var offset = (Number(args.offset) + change);
     //render last page if offset is greater than results pages
-    if (offset > pages.length){
-        offset = pages.length;
+    if (offset > pageArray.length){
+        offset = pageArray.length;
     } else if (offset < 0){
         offset = 0;
     }
@@ -89,19 +93,62 @@ function showPage(pageArray, args, change, res){
     console.log(items);
     //concatenate the content
     var cont = '';
-    for(var i = 0; i < items.length; i++){
-        cont += ('<div class="result">' +
-        '<h2>' + items[i].title + '</h2>' +
-        '<p>Views: ' + items[i].views + '</p>' +
-        '<p><a target="blank" href="' + items[i].url + '">' + items[i].url + '</a></p></div>');
+    if(items){
+        for(var i = 0; i < items.length; i++){
+            cont += ('<div class="result">' +
+            '<h2>' + items[i].title + '</h2>' +
+            '<p>Views: ' + items[i].views + '</p>' +
+            '<p><a target="blank" href="' + items[i].url + '">' + items[i].url + '</a></p></div>');
+        }
+    } else {
+        cont = 'Nothing found';
+    }
+    //keep the checked search method
+    var checked = {recent: true, top: false};
+    if (args.sort === 'top'){
+        checked = {recent: false, top: true};
     }
     res.render('index', {
         content: cont,
-        searchTerm: args.subreddit
+        searchTerm: args.subreddit,
+        checkedRecent: checked.recent,
+        checkedTop: checked.top
     });
 }
 
 
+//search history functions
+function saveSearch(args){
+    var search = {
+        time: new Date,
+        subreddit: args.subreddit,
+        sort: args.sort
+    }
+    mongo.connect(mongoURI, function(err, db){
+        db.collection('searches').insert({
+            search
+        });
+    })
+}
+
+function getSearches(req, res){
+    var history = [];
+    mongo.connect(mongoURI, function(err, db){
+        if (err) throw err;
+        db.collection('searches').find().toArray(function(err, data){
+            data.forEach(function(doc){
+                var url = ('/r/' + doc.search.subreddit + '?sort=' + doc.search.sort);
+                history.push('<p><a href="' + url + '">' + doc.search.subreddit + ' sorted by ' + doc.search.sort + '</a> at ' + doc.search.time + '</p>');
+            });
+            res.render('index', {
+                content: history.reverse().join(''),
+                searchTerm: '',
+                checkedRecent: true,
+                checkedTop: false
+            });
+        });
+    });
+}
 
 //ROUTES
 //--/ = index
@@ -116,16 +163,14 @@ function showPage(pageArray, args, change, res){
 app.get('/', function(req, res){
     res.render('index', {
         content: '<p>Welcome to imgureddit! You can search for a subreddit in the search bar above, or navigate directly to a URL like so:</p>       <p>/r/(subreddit)?sort=(recent OR top)&offset=(page number)</p>',
-        searchTerm: 'testing stuff'
+        searchTerm: '',
+        checkedRecent: true,
+        checkedTop: false
     });
     res.end();
 });
 
 app.get(/^\/r\/(.+)(\\?(.+))?$/, function(req, res){
-    //DEBUG
-    console.log('got the params query from a GET');
-    console.log(req.params);
-    console.log(req.query);
     var args = {
         subreddit: req.params[0],
         offset: '0',
@@ -152,6 +197,9 @@ app.post('/*', function(req, res){
     res.redirect('/r/' + args.subreddit + '?sort=' + args.sort + '&offset=' + args.offset);
 });
 
-
+app.get('/history', function(req, res){
+    //show search history
+    getSearches(req, res);
+})
 
 app.listen(process.env.PORT || 8080);
